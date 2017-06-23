204f0816f29b2f3f7aa22900eb108292697a6719
Ubuntu <ubuntu@ip-172-31-7-150.ap-northeast-1.compute.internal>
2017-06-23 01:46:08 +0000

    harusame

--- yahooNewsParserFromRSS.py
+++ yahooNewsParserFromRSS.py
@@ -90,20 +90,22 @@ def _driver(array):
   return None
 
 def _local_driver(array):
-
-  time_dirname, link, contexttype = array  
-  filename = link.replace('/', '_')
-  if os.path.isfile('output/%s/%s'%(time_dirname, filename)) is True:
-    return None
-  tp = html_adhoc_fetcher(link)
-  if tp == None:
-    return None
-  title, contents_all_text = tp
-  print("link", link)
-  print(title) 
-  #print("contents all text ", contents_all_text)
-  with open('output/%s/%s'%(time_dirname, filename), 'w') as f:
-    f.write(contents_all_text)
+  try:
+    time_dirname, link, contexttype = array  
+    filename = link.replace('/', '_')
+    if os.path.isfile('output/%s/%s'%(time_dirname, filename)) is True:
+      return None
+    tp = html_adhoc_fetcher(link)
+    if tp == None:
+      return None
+    title, contents_all_text = tp
+    print("link", link)
+    print(title) 
+    #print("contents all text ", contents_all_text)
+    with open('output/%s/%s'%(time_dirname, filename), 'w') as f:
+      f.write(contents_all_text)
+  except Exception as e:
+    print(e)
   return None
 
 #COnfig slack 
